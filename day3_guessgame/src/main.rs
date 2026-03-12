b0ccf1c9397887c90cd78c93a248e960d26e5546
Kaushal Goud <kaushalgoud82@gmail.com>
2026-03-12 23:46:54 +0545

    hello

--- day3_guessgame/src/main.rs
+++ day3_guessgame/src/main.rs
@@ -1,20 +1,41 @@
-// Guessing Game
+use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
 fn main() {
-    println!("Guess the number!");
-    let random_number = 10;
-    let mut input: String = String::new();
+    println!("Guess the number between 1 to 10!");
+
+    let random_number = rand::thread_rng().gen_range(1..=10);
+
     loop {
-        println!("What's your guess?");
-        io::stdin().read_line(&mut input);
-        let mut guess_number = input.trim().parse().unwrap();
-        if random_number == guess_number {
-            println!("Correct Guess!!! ie.{}",guess_number);
+        let mut input = String::new();
+
+        print!("What's your guess? huh->");
+
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Error reading input");
+
+        let input: i32 = input.trim().parse().expect("Please type a number!");
+
+        println!("Your Guess: {}", input);
+
+        if input == random_number {
+            println!("Correct Guess!!! ie. {}", input);
             break;
+        } else {
+
+            print!("😭");
+            println!(" Try Again!! ");
+            print!("Correct Answer :{random_number}\n");
         }
-        else {
-            input.clear();
-            println!("😭Try Again!!");
+
+        match input.cmp(&random_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You nailed it");
+                break;
+            }
         }
     }
 }
